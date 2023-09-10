57aac6d3230b81aac8d5870ff085314a71fafc8c
David Kadish <david.kadish@gmail.com>
2023-09-11 01:04:17 +0200

    first commit

---
+++ lib/AudioTestLibrary/include/AudioTestLibrary.h
@@ -0,0 +1,21 @@
+//
+// Created by David Kadish on 10/09/2023.
+//
+
+#ifndef AUDIOTESTLIBRARY_H
+#define AUDIOTESTLIBRARY_H
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+
+class AudioTestClass {
+
+public:
+    void setup();
+
+    void loop();
+};
+
+
+#endif //AUDIOTESTLIBRARY_H
---
+++ lib/AudioTestLibrary/src/AudioTestLibrary.cpp
@@ -0,0 +1,11 @@
+//
+// Created by David Kadish on 10/09/2023.
+//
+
+#include "AudioTestLibrary.h"
+
+AudioTestClass::AudioTestClass() {}
+
+void AudioTestClass::loop() {}
+
+void AudioTestClass::setup() {}
